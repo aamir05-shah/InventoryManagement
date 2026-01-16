cc29ac7619e60a35ec0d025ecabc5ba4e669ff6d
unknown <aamir05.shah@gmail.com>
2026-01-17 00:08:03 +0530

    Remove Crispy forms

--- config/settings.py
+++ config/settings.py
@@ -40,10 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'crispy_forms',
-    'crispy_bootstrap5',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -127,9 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
